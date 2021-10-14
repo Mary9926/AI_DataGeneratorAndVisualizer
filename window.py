047192c11b90086d9708404e2302199cfb2e5805
Maria Gryglewska <maria.gryglewska51@gmail.com>
2021-10-14 22:26:00 +0200

    fixes

--- window.py
+++ window.py
@@ -1,4 +1,5 @@
 import random
+
 from matplotlib import pyplot as plt
 import tkinter
 import numpy as np
@@ -6,9 +7,12 @@ import numpy as np
 root = tkinter.Tk()
 root.title('Data')
 root.geometry('500x500')
+modesAmount = 1
+samplesAmount = 4
 
 
-def generateSamples(xClass, yClass, samplesAmount, devRange):
+def generateSamples(xClass, yClass, samplesAmount):
+    devRange = random.uniform(0, 1)
     xClassNormal = np.random.normal(xClass, devRange, samplesAmount)
     yClassNormal = np.random.normal(yClass, devRange, samplesAmount)
     return xClassNormal, yClassNormal
@@ -16,7 +20,7 @@ def generateSamples(xClass, yClass, samplesAmount, devRange):
 
 def generateModes(modesAmount):
     rng = np.random.default_rng()
-    rangeFirst = 0
+    rangeFirst = -1
     rangeLast = 1
     xClass = rng.uniform(rangeFirst, rangeLast, modesAmount)
     yClass = rng.uniform(rangeFirst, rangeLast, modesAmount)
@@ -28,20 +32,15 @@ def plot():
     fig = plt.figure()
     ax = fig.add_subplot()
     ax.set(title='Data Visualizer - modes and samples', xlabel='x', ylabel='y')
-    modesAmount = 1
-    samplesAmount = 4
-    devRange = 0.25
 
     x, y, label = generateModes(modesAmount)
-    xn, yn = generateSamples(x, y, samplesAmount, devRange)
-
+    xn, yn = generateSamples(x, y, samplesAmount)
     for i in range(modesAmount):
         ax.scatter(x, y, color='magenta', marker='s', label='Numbers')
         ax.scatter(xn, yn, color='magenta', marker='.')
 
     x, y, label = generateModes(modesAmount)
-
-    xn, yn = generateSamples(x, y, samplesAmount, devRange)
+    xn, yn = generateSamples(x, y, samplesAmount)
     for i in range(modesAmount):
         ax.scatter(x, y, color='cyan', marker='s')
         ax.scatter(xn, yn, color='cyan', marker='.')
