5cc4b8088fd6021eaa7cb8f24fe5b53ccf802c0f
Maria Gryglewska <maria.gryglewska51@gmail.com>
2021-11-18 17:58:12 +0100

    Added linear and activation layer and schema of NeuralNetwork class

--- neuron.py
+++ neuron.py
@@ -5,7 +5,7 @@ from window import *
 class Neuron:
     epochs = 10000
     epsilon = 0.00001
-    learningRate = 0.0001
+    learningRate = 0.1
 
     def __init__(self, activationFunction, activationFunctionDerivative):
         rng = np.random.default_rng()
@@ -38,6 +38,78 @@ class Neuron:
         return self.activationFunction(self.neuronState(inputSamples))
 
 
+
+
+class NeuralNetwork:
+    layers = 3
+
+    def __init__(self, layers):
+        rng = np.random.default_rng()
+        weights = []
+        self.layers = layers
+
+        for i in range(len(layers) - 1 ):
+            w = rng.random(layers[i] + 1, layers[i] - 1) # weights in range (-1, 1)
+            weights.append(w)
+            self.weights = weights
+
+    def backPropagation(self): # for training
+        # for i in np.arange(0,len(leyers) - 2):
+        #     w = np.random.randn(layers[i] + 1, layers[i + 1] + 1 )
+        #     self.W.append(w/ np.sqrt(layers[i]))
+
+
+
+    def forwardPropagation(self, inputSamples): # for evaluation give us a prediction output predicted by network
+        for i in enumerate(self.weights):
+            networkInputs = inputSamples @ weigths
+            activations = self.activationFunction(networkInputs)
+            print('activations')
+            print(activations)
+            return activations
+
+    def gradientOfActivationFunction(self, learningRate):
+        for i in range(len(self.weights)):
+            weights = self.weights[i]
+            activationFunctionDerivative = self.activationFunctionDerivative[i]
+            weights+= activationFunctionDerivative + learningRate
+
+
+
+class Linear:
+    neuronNumber = 2
+    def __init__(self):
+        self.inputSamples = []
+        self.neuronNumber = neuronNumber
+
+    def forwardPropagation(self, inputSamples):
+        self.inputSamples = np.c_[inputSamples, np.ones(len(inputSamples)) * -1]
+        return  inputSamples @ weigths #state
+
+    def backPropagation(self, gradientOfLoss):
+        self.gradientOfLoss = gradientOfLoss
+        return gradientOfLoss @ weights
+
+    def adjust(self, eta): # here ask
+        self.weights += eta * gradientOfLoss * inputSamples
+
+
+class Activation:
+
+    def __init__(self, state):
+        self.state = state
+
+    def forwardPropagation(self, state):
+       self.state = state
+       return sigmoid(state)
+
+    def backPropagation(self, gradientOfLoss):
+        return gradientOfLoss * sigmoidDerivative(state)
+
+    def adjust(self, eta):
+        pass
+
+
 def sigmoid(s):
     b = -6
     return 1.0 / (1 + np.exp(b * s))
@@ -46,28 +118,4 @@ def sigmoid(s):
 def sigmoidDerivative(s):
     return sigmoid(s) * (1 - sigmoid(s))
 
-
-def heaviside(s):
-    threshold = 0.5
-    return np.heaviside(s, threshold)
-
-
-def heavisideDerivative(s):
-    return np.ones(s.shape)
-
-
-def sin(s):
-    return np.sin(s)
-
-
-def sinDerivative(s):
-    return np.cos(s)
-
-
-def tanh(s):
-    return np.tanh(s)
-
-
-def tanhDerivative(s):
-    return 1 - (np.tanh(s) * np.tanh(s))
 
